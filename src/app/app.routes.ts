4db95ae2ae635c7fa5b7c7876fa1fe79bf351ad6
Sakhilanandan <akha9923@gmail.com>
2026-04-06 18:23:48 +0530

    Initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { BookingsComponent } from './bookings/bookings';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: BookingsComponent }
+];
